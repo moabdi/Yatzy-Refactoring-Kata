f864106124c4741df6d7c33b1841ec5c4cf65e61
mabdi <mostapha.abdi@netseenergy.fr>
2020-01-31 11:15:56 +0100

    init categories

---
+++ java/src/main/java/moabdi/yatzy/kata/Category.java
@@ -0,0 +1,20 @@
+package moabdi.yatzy.kata;
+
+public enum Category {
+  YATZY,
+  CHANCE,
+  ONES,
+  TWOS,
+  THREES,
+  FOURS,
+  FIVES,
+  SIXES,
+  PAIR,
+  TWO_PAIRS,
+  THREE_OF_A_KIND,
+  FOUR_OF_A_KIND,
+  FULL_HOUSE,
+  SMALL_STRAIGHT,
+  LARGE_STRAIGHT;
+
+}
