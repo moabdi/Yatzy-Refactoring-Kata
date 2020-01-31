00770d81a11c298b96525ca81c2692cb9a69f8b0
mabdi <mostapha.abdi@netseenergy.fr>
2020-01-31 11:25:37 +0100

    define yatzy rules

--- java/src/main/java/moabdi/yatzy/kata/Category.java
+++ java/src/main/java/moabdi/yatzy/kata/Category.java
@@ -1,20 +1,76 @@
 package moabdi.yatzy.kata;
 
-public enum Category {
-  YATZY,
-  CHANCE,
-  ONES,
-  TWOS,
-  THREES,
-  FOURS,
-  FIVES,
-  SIXES,
-  PAIR,
-  TWO_PAIRS,
-  THREE_OF_A_KIND,
-  FOUR_OF_A_KIND,
-  FULL_HOUSE,
-  SMALL_STRAIGHT,
-  LARGE_STRAIGHT;
+import static java.util.Arrays.asList;
+import static java.util.Collections.frequency;
+import static moabdi.yatzy.kata.Dice.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public enum Category {
+
+  YATZY(allDiceAreEqual()),
+  CHANCE(matchAlways()),
+  ONES(contains(ONE)),
+  TWOS(contains(TWO)),
+  THREES(contains(THREE)),
+  FOURS(contains(FOUR)),
+  FIVES(contains(FIVE)),
+  SIXES(contains(SIX)),
+  PAIR(hasXTimesPair(1)),
+  TWO_PAIRS(hasXTimesPair(2)),
+  THREE_OF_A_KIND(hasXTimes(3)),
+  FOUR_OF_A_KIND(hasXTimes(4)),
+  FULL_HOUSE(hasFullHouse()),
+  SMALL_STRAIGHT(allDiceMatch(asList(ONE, TWO, THREE, FOUR, FIVE))),
+  LARGE_STRAIGHT(allDiceMatch(asList(TWO, THREE, FOUR, FIVE, SIX)));
+
+  private Predicate<List<Dice>> rule;
+
+  private Category(Predicate<List<Dice>> rule) {
+    this.rule = rule;
+  }
+
+  public boolean evaluate(Dice... dice) {
+    return evaluate(Arrays.asList(dice));
+  }
+
+  public boolean evaluate(List<Dice> dice) {
+    return rule.test(dice);
+  }
+
+  private static Predicate<List<Dice>> matchAlways() {
+    return dice -> true;
+  }
+
+  private static Predicate<List<Dice>> contains(Dice die) {
+    return dice -> dice.contains(die);
+  }
+
+  private static Predicate<List<Dice>> hasXTimes(int times) {
+    return dice -> dice.stream().anyMatch(die -> dice.stream().filter(d -> d == die).count() >= times);
+  }
+
+  private static Predicate<List<Dice>> allDiceAreEqual() {
+    return dice -> dice.stream().allMatch(die -> die == dice.get(0));
+  }
+
+  private static Predicate<List<Dice>> hasXTimesPair(int times) {
+    return dice -> dice.stream()
+        .filter(current -> frequency(dice, current) >= 2)
+        .collect(Collectors.toSet()).size()>=times;
+  }
+
+  private static Predicate<List<Dice>> hasFullHouse() {
+    return dice -> dice.stream()
+        .filter(current -> frequency(dice, current) >= 2)
+        .collect(Collectors.toSet()).size() == new HashSet<>(dice).size();
+  }
+
+  private static Predicate<List<Dice>> allDiceMatch(List<Dice> dices) {
+    return dice -> new HashSet<>(dice).size() == dices.size();
+  }
 }
---
+++ java/src/main/test/moabdi/yatzy/kata/CategoryTest.java
@@ -0,0 +1,101 @@
+package moabdi.yatzy.kata;
+
+import org.junit.Test;
+
+import static moabdi.yatzy.kata.Category.*;
+import static moabdi.yatzy.kata.Dice.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CategoryTest {
+
+  @Test
+  public void shouldEvaluateContainsReturnTrue() {
+    assertTrue(ONES.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertTrue(TWOS.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertTrue(THREES.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertTrue(FOURS.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertTrue(FIVES.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertTrue(SIXES.evaluate(ONE, TWO, THREE, FOUR, SIX));
+  }
+
+  @Test
+  public void shouldEvaluateContainsReturnFalse() {
+    assertFalse(ONES.evaluate(TWO, THREE, FOUR, FIVE, SIX));
+    assertFalse(TWOS.evaluate(ONE, THREE, FOUR, FIVE, SIX));
+    assertFalse(THREES.evaluate(ONE, TWO, FOUR, FIVE, SIX));
+    assertFalse(FOURS.evaluate(ONE, TWO, THREE, FIVE, SIX));
+    assertFalse(FIVES.evaluate(ONE, TWO, THREE, FOUR, SIX));
+    assertFalse(SIXES.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+  }
+
+
+  @Test
+  public void shouldEvaluateHasXTimesPairReturnTrue() {
+    assertTrue(PAIR.evaluate(TWO, TWO, FOUR, FIVE, SIX));
+    assertTrue(TWO_PAIRS.evaluate(TWO, TWO, FOUR, FOUR, SIX));
+  }
+
+  @Test
+  public void shouldEvaluateHasXTimesPairReturnFalse() {
+    assertFalse(PAIR.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertFalse(TWO_PAIRS.evaluate(ONE, ONE, THREE, FOUR, FIVE));
+  }
+
+  @Test
+  public void shouldEvaluateHasXTimesReturnTrue() {
+    assertTrue(THREE_OF_A_KIND.evaluate(TWO, TWO, TWO, FIVE, SIX));
+    assertTrue(FOUR_OF_A_KIND.evaluate(FOUR, FOUR, FOUR, FOUR, SIX));
+  }
+
+  @Test
+  public void shouldEvaluateHasXTimesReturnFalse() {
+    assertFalse(THREE_OF_A_KIND.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+    assertFalse(FOUR_OF_A_KIND.evaluate(ONE, ONE, THREE, FOUR, FIVE));
+  }
+
+  @Test
+  public void shouldEvaluateHasFullHouseReturnTrue() {
+    assertTrue(FULL_HOUSE.evaluate(ONE, ONE, ONE, FOUR, FOUR));
+  }
+
+  @Test
+  public void shouldEvaluateHasFullHouseReturnFalse() {
+    assertFalse(FULL_HOUSE.evaluate(ONE, ONE, ONE, TWO, THREE));
+  }
+
+  @Test
+  public void shouldEvaluateSmallStraightReturnTrue() {
+    assertTrue(SMALL_STRAIGHT.evaluate(ONE, TWO, THREE, FOUR, FIVE));
+  }
+
+  @Test
+  public void shouldEvaluateSmallStraightReturnFalse() {
+    assertFalse(SMALL_STRAIGHT.evaluate(ONE, ONE, ONE, TWO, THREE));
+  }
+
+  @Test
+  public void shouldEvaluateLargeStraightReturnTrue() {
+    assertTrue(LARGE_STRAIGHT.evaluate(TWO, THREE, FOUR, FIVE, SIX));
+  }
+
+  @Test
+  public void shouldEvaluateLargeStraightReturnFalse() {
+    assertFalse(LARGE_STRAIGHT.evaluate(ONE, ONE, ONE, TWO, THREE));
+  }
+
+  @Test
+  public void shouldEvaluateYatzyReturnTrue() {
+    assertTrue(YATZY.evaluate(TWO, TWO, TWO, TWO, TWO));
+  }
+
+  @Test
+  public void shouldEvaluateYatzyReturnFalse() {
+    assertFalse(YATZY.evaluate(ONE, ONE, ONE, TWO, THREE));
+  }
+
+  @Test
+  public void shouldEvaluateChanceReturnTrue() {
+    assertTrue(CHANCE.evaluate(ONE, TWO));
+  }
+}
